21849daa8e6e15f472e9bcdbc09dd85693561237
firasabualkomboz <firasa018@gmail.com>
2021-04-27 00:34:13 +0300

    ya qodosdddddsss

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,20 @@
-$(document).ready(function (){
+// $(document).ready(function (){
 
 
-    $("body").niceScroll();
+//     $("body").niceScroll();
     
-    $('.carousel').carousel({
-        interval:100
-    });
-});
+//     $('.carousel').carousel({
+//         interval:100
+//     });
+// });
+
+
+    var text = ["Developer", "Designer", "Covert design","website design","backend","PHP","FRONT-END","OTHERS"];
+    var counter = 0;
+    var elem = document.getElementById("changeText");
+    setInterval(change, 300);
+    function change() {
+     elem.innerHTML = text[counter];
+        counter++;
+        if(counter >= text.length) { counter = 0; }
+    }
