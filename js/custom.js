22d9acabb6970b1ad0d4cb0f9cba6d183216f2d7
firasabualkomboz <firasa018@gmail.com>
2021-08-13 14:10:30 +0300

    there is some updated

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@
     var text = ["Developer", "Designer", "Covert design","website design","backend","PHP","FRONT-END","OTHERS"];
     var counter = 0;
     var elem = document.getElementById("changeText");
-    setInterval(change, 300);
+    setInterval(change, 500);
 
     function change() {
      elem.innerHTML = text[counter];
